cfef0ecfdd8baf652026499c2edac394738de697
bmt1993 <bmt@outlook.com.ar>
2023-12-01 14:20:43 -0300

    Initial commit

---
+++ src/proyectoa/Clase1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoa;
+
+/**
+ *
+ * @author bmt
+ */
+public class Clase1 {
+    
+    private int atributoA;
+
+    public Clase1(int atributoA) {
+        this.atributoA = atributoA;
+    }
+
+    public int getAtributoA() {
+        return atributoA;
+    }
+
+    public void setAtributoA(int atributoA) {
+        this.atributoA = atributoA;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
